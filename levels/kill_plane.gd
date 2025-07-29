97807d4a996c55c430c3a89d68121d9331a2bb57
Ali <d.a.bhatti@hotmail.com>
2025-07-29 15:14:18 +0100

    update forcefield colour in real time

--- levels/kill_plane.gd
+++ levels/kill_plane.gd
@@ -1,5 +1,5 @@
 extends Area3D
 
 
-func _on_body_entered(body: Node3D) -> void:
+func _on_body_entered(_body: Node3D) -> void:
 	get_tree().reload_current_scene.call_deferred()
--- levels/terrain_manager.gd
+++ levels/terrain_manager.gd
@@ -1,46 +1,48 @@
-@tool
 extends Node3D
 
-@onready var floor: CSGBox3D = %Floor
+@onready var gamefloor: CSGBox3D = %Floor
 
 @export var terrain_scene: PackedScene
 @export var instance_count: int = 1
 @export var terrain_width: float = 0
 @export var terrain_depth: float = 0
-@export var forcefield_colour: Color = Color(0.0, 0.6, 1.0):
-	set(value) : update_forcefield(value)
-	
+
+# Color property with setter logic and backing variable to avoid recursion
+var _forcefield_colour: Color = Color(0.0, 0.6, 1.0, 1.0)
+@export var forcefield_colour: Color = Color(0.0, 0.6, 1.0, 1.0):
+	get: return _forcefield_colour
+	set(value):
+		if _forcefield_colour != value:
+			_forcefield_colour = value
+			update_forcefield(value)
+
 var rng = RandomNumberGenerator.new()
 var shader_material = ShaderMaterial.new()
-var terrain_instances : Array = [] 
+var terrain_instances: Array = []
+
 
 func update_forcefield(value: Color):
-	print("updating forcefield")
-	print("value: ", value)
+	
 	for terrain in terrain_instances:
-		print(terrain.name)
-		if terrain.has_node("Mesh"):
-			print("found mesh")
-			
-		if terrain.get_node("Mesh").get_surface_override_material(0) is ShaderMaterial:
-			print("found shader material")
-			
-		if terrain.has_node("Mesh") and terrain.get_node("Mesh").get_surface_override_material(0):
+		
+		# Don't update shader till mesh has initialised
+		if terrain.has_node("Mesh") and terrain.get_node("Mesh").mesh != null:
 			print(forcefield_colour)
 			terrain.get_node("Mesh").get_active_material(0).set_shader_parameter("glow_color", value)
 
 
-
-
+# Called when the node is added to the scene; scales terrain to floor size
 func _ready():
-	#scale to size of floor
-	terrain_width = floor.size[0]
-	terrain_depth = floor.size[2]
-
+	terrain_width = gamefloor.size[0]
+	terrain_depth = gamefloor.size[2]
+	
+	# Set color at runtime
+	forcefield_colour = _forcefield_colour 
+	
 	generate_central_dome()
 
 
-#use this to generate lots of structures placed according to some rules
+# Use this to generate lots of structures placed according to some rules
 func generate_terrain_procedurally():
 	if not terrain_scene:
 		push_error("No terrain scene assigned.")
@@ -60,26 +62,23 @@ func generate_terrain_procedurally():
 		root.add_child(instance)
 
 
-#manually generate large central dome with forcefield like appearance
+# Manually generate large central dome with forcefield-like appearance
 func generate_central_dome():
-	
 	if not terrain_scene:
 		push_error("No terrain scene assigned.")
 		return
 
 	var root = $TerrainRoot
-	
 	var instance = terrain_scene.instantiate()
-	var x = 0
-	var z = 0
-	instance.position = Vector3(x, 0, z)
-	
+	instance.position = Vector3(0, 0, 0)
+
 	terrain_instances.append(instance)
 	root.add_child(instance)
-	
+
 	shader_material.shader = load("res://levels/structures/forcefield.gdshader")
 	shader_material.set_shader_parameter("glow_color", forcefield_colour)
-	instance.get_node("Mesh").set_surface_override_material(0, shader_material)
 
-
-	
+	if instance.has_node("Mesh"):
+		var mesh_node = instance.get_node("Mesh")
+		if mesh_node.mesh and mesh_node.mesh.get_surface_count() > 0:
+			mesh_node.set_surface_override_material(0, shader_material)
